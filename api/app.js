8385604486a76ac2bc953b5d9e3699702c279a42
Michael Theodorides <mtheodorides@users.noreply.github.com>
2016-12-02 21:01:52 -0800

    Update app.js

--- api/app.js
+++ api/app.js
@@ -36,7 +36,7 @@ app.get('/removeFood', function(req, res){
 	db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='foods'", function(error, row) {
 		if (row !== undefined) {
 			console.log("table exists. cleaning existing records");
-			db.run("DELETE FROM Table23 WHERE username=(?) AND food=(?)",username, food);
+			db.run("DELETE FROM food WHERE username=(?) AND food=(?)",username, food);
 		}
 		else {
 			console.log("creating table");
