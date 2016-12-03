4caccb232546503687322cff4cc1f466ca3b6799
Michael Theodorides <michael.theodorides@gmail.com>
2016-12-03 00:42:11 -0800

    added simple web server

---
+++ webserver/app.js
@@ -0,0 +1,39 @@
+/******NO NEED TO MODIFY ****/
+var express = require('express'); // Adding the express library 
+var mustacheExpress = require('mustache-express'); // Adding mustache templating system and connecting it to 
+var request = require('request')  // Adding the request library (to make HTTP reqeusts from the server)
+var tools = require('./tools.js'); // Custom module providing additional functionality to the server
+var bodyParser = require('body-parser');
+
+var app = express(); // initializing applicaiton
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+// For each request to this server, run the function "logger" in tools.js 
+app.use(tools.logger);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up /static path to host css/js/image files directly
+app.use('/static', express.static(__dirname + '/static'));
+/****END OF NO NEED TO MODIFY SECTION ******/
+
+
+// Define your routes here
+app.get('/', function (req, res, next) {
+  res.render('index.html', { });
+});
+app.get('/cookery', function (req, res, next) {
+  res.render('cookery.html', { });
+});
+
+
+
+  
+// Start up server on port 3000 on host localhost
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+
+  console.log('Assignment 2 server on localhost listening on port ' + port + '!');
+  console.log('Open up your browser (within your VM) and enter the URL "http://localhost:' + port + '" to view your website!');
+});
